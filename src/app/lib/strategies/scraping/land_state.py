be0af191074864e5db93bf3e22d8c91a3445770a
Jairo <jairo.araujo.silva@gmail.com>
2024-04-04 00:15:06 -0300

    wip

--- src/app/lib/strategies/scraping/land_state.py
+++ src/app/lib/strategies/scraping/land_state.py
@@ -1,6 +1,7 @@
 import asyncio
-import datetime
 import json
+from datetime import datetime, timedelta
+from random import randint
 from typing import Union
 
 import redis
@@ -28,6 +29,26 @@ class LandState:
     @property
     def state(self):
         return self._state
+
+    @property
+    def last_tree_respawn_in(self) -> timedelta:
+        try:
+            return self._get_last_tree_respawn_in()
+        except Exception:
+            return timedelta(seconds=randint(180, 300))
+
+    def _get_last_tree_respawn_in(self) -> timedelta:
+        entities: dict = self.state["entities"]
+        max_utc_refresh = max(
+            [
+                value["generic"]["utcRefresh"]
+                for key, value in entities.items()
+                if value["entity"].startswith("ent_tree")
+            ]
+        )
+        last_utc_refresh = datetime.fromtimestamp(max_utc_refresh / 1000)
+        now = datetime.now()
+        return last_utc_refresh - now
 
     @classmethod
     async def from_browser(cls, land_number: int) -> "LandState":
@@ -84,7 +105,7 @@ class LandState:
         return None
 
     @classmethod
-    def get(cls, land_number: int, cached: bool = False):
+    def get(cls, land_number: int, cached: bool = True):
         if cached and (land_state := cls.from_cache(land_number)):
             return land_state
 
@@ -105,7 +126,7 @@ class LandState:
         )
 
     @classmethod
-    def enqueue_in(cls, land_number: int, at: datetime.timedelta) -> rq.job.Job:
+    def enqueue_in(cls, land_number: int, at: timedelta) -> rq.job.Job:
         return cls.queue.enqueue_in(
             at,
             worker,
@@ -124,5 +145,5 @@ async def phaser_land_state_getter(page: Page):
 
 def worker(land_number: int):
     land_state: LandState = asyncio.run(LandState.from_browser(land_number))
-
+    LandState.enqueue_in(land_number, land_state.last_tree_respawn_in)
     return json.dumps(land_state.state)
