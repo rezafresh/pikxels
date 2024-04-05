4c5d09912d9a60c610ab1ece7f527a1b7a36cebf
Jairo <jairo.araujo.silva@gmail.com>
2024-04-05 14:04:02 -0300

    wip

--- src/app/cli/tree_hunt.py
+++ src/app/cli/tree_hunt.py
@@ -1,9 +1,9 @@
-from ..lib.strategies.scraping import LandState
+from ..lib.strategies.scraping.land_state import enqueue as land_state_enqueue
 
 
 def main():
     for i in range(5000):
-        LandState.enqueue(i + 1)
+        land_state_enqueue(i + 1)
 
 
 if __name__ == "__main__":
