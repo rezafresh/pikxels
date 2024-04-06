a210aa3f50f1b563082e2d53bde5fe334e96d615
Jairo <jairo.araujo.silva@gmail.com>
2024-04-06 20:20:25 -0300

    wip

--- src/app/cli/start_workers.py
+++ src/app/cli/start_workers.py
@@ -14,7 +14,9 @@ def main():
     default_queue_workers = math.floor(concurrency * 0.6)
     low_queue_workers = concurrency - default_queue_workers
     workers = [rq.Worker([q.default], connection=q._redis) for _ in range(default_queue_workers)]
-    workers.extend(rq.Worker([q.low], connection=q._redis) for _ in range(low_queue_workers))
+    workers.extend(
+        rq.Worker([q.low, q.default], connection=q._redis) for _ in range(low_queue_workers)
+    )
     processes = [Process(target=w.work) for w in workers]
 
     for p in processes:
