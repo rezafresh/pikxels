151d78fb81894cee376c4769aef9a54da56e23ea
Jairo <jairo.araujo.silva@gmail.com>
2024-04-09 04:04:14 -0300

    wip

--- src/app/cli/start_workers.py
+++ src/app/cli/start_workers.py
@@ -8,6 +8,10 @@ import rq.scheduler
 from ..lib.strategies.scraping import _queues as q
 
 
+def create_workers(queues: list[rq.Queue], count: int) -> list[rq.Worker]:
+    return [rq.Worker(queues, connection=q._redis, default_worker_ttl=-1) for _ in range(count)]
+
+
 def main():
     if not (concurrency := int(os.getenv("BROWSERLESS_MAX_CONCURRENT_SESSIONS", 0))):
         raise Exception("BROWSERLESS_MAX_CONCURRENT_SESSIONS env variable is not defined")
@@ -16,8 +20,8 @@ def main():
     sync_queue_workers = concurrency - default_queue_workers
 
     workers = [
-        *[rq.Worker([q.default], connection=q._redis) for _ in range(default_queue_workers)],
-        *[rq.Worker([q.sync], connection=q._redis) for _ in range(sync_queue_workers)],
+        *create_workers([q.default], default_queue_workers),
+        *create_workers([q.sync], sync_queue_workers),
     ]
     processes = [Process(target=w.work) for w in workers]
 
