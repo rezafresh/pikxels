b99ee8f9022dbba352d9e4a24de3c05a39ed29ed
Jairo <jairo.araujo.silva@gmail.com>
2024-04-14 01:50:27 -0300

    wip

--- src/app/api/services.py
+++ src/app/api/services.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from itertools import chain
 from time import time
@@ -6,7 +7,7 @@ import httpx
 import rq
 from fastapi import HTTPException
 
-from ..lib.strategies.scraping._queues import sync
+from ..lib.strategies.scraping._queues import _redis, sync
 from ..lib.strategies.scraping.land_state import get as land_state_get
 from ..lib.strategies.scraping.land_state import parse as land_state_parse
 
@@ -19,22 +20,34 @@ def get_land_state(land_number: int, cached: bool = True, raw: bool = False):
 
 
 def get_cached_lands_available_resources(offset: int = 0):
-    if not (job_ids := sync.finished_job_registry.get_job_ids()):
-        return HTTPException(404, "No data found")
+    if not (cached := _redis.get("app:lands:resources")):
+        if not (job_ids := sync.finished_job_registry.get_job_ids()):
+            return HTTPException(404, "No data found")
 
-    jobs = [sync.fetch_job(jid) for jid in job_ids]
-    now = datetime.now()
+        jobs = [sync.fetch_job(jid) for jid in job_ids]
 
-    def parse(j: rq.job.Job):
-        if last_result := j.latest_result():
-            if result := last_result.return_value:
-                return {
-                    "lastUpdated": last_result.created_at.astimezone().isoformat(),
-                    **land_state_parse(result),
-                }
+        def parse(j: rq.job.Job):
+            if last_result := j.latest_result():
+                if result := last_result.return_value:
+                    return {
+                        "lastUpdated": last_result.created_at.astimezone().isoformat(),
+                        **land_state_parse(result),
+                    }
+            return None
+
+        resources = {j.args[0]: parse(j) for j in jobs}
+        _redis.set("app:lands:resources", json.dumps(resources, default=str), ex=60)
+    else:
+        resources = json.loads(cached)
+
+    def to_datetime(dt: datetime | str | None) -> datetime | None:
+        if isinstance(dt, datetime):
+            return dt
+        elif isinstance(dt, str):
+            return datetime.strptime(dt, "%Y-%m-%d %H:%M:%S")
         return None
 
-    resources = {j.args[0]: parse(j) for j in jobs}
+    now = datetime.now()
     resources = [
         [
             *[
@@ -46,7 +59,7 @@ def get_cached_lands_available_resources(offset: int = 0):
                     **t,
                 }
                 for t in state["trees"]
-                if 0 < ((t["utcRefresh"] or now) - now).total_seconds() < 600
+                if 0 < ((to_datetime(t["utcRefresh"]) or now) - now).total_seconds() < 600
             ],
             *[
                 {
@@ -57,13 +70,16 @@ def get_cached_lands_available_resources(offset: int = 0):
                     **w,
                 }
                 for w in state["windmills"]
-                if 0 < ((w["finishTime"] or now) - now).total_seconds() < 600
+                if 0 < ((to_datetime(w["finishTime"]) or now) - now).total_seconds() < 600
             ],
         ]
         for land_number, state in resources.items()
     ]
     resources = [*chain.from_iterable(resources)]
-    resources = sorted(resources, key=lambda r: (r.get("utcRefresh") or r.get("finishTime") or now))
+    resources = sorted(
+        resources,
+        key=lambda r: (to_datetime(r.get("utcRefresh")) or to_datetime(r.get("finishTime")) or now),
+    )
     return {
         "totalItems": len(resources),
         "currentOffset": offset,
