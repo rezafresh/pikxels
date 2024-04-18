c8b1ab4b42c7c7a267bf845dd014fcdd9b7f30cc
Jairo <jairo.araujo.silva@gmail.com>
2024-04-18 13:14:21 -0300

    wip

--- src/app/api/router.py
+++ src/app/api/router.py
@@ -4,4 +4,4 @@ from . import controllers as ctrls
 
 router = APIRouter()
 router.get("/land/{land_number:int}/state/")(ctrls.get_land_state)
-router.websocket("/land/states/stream/")(ctrls.stream_lands_states)
+router.websocket("/lands/states/stream/")(ctrls.stream_lands_states)
